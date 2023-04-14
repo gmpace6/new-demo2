d8cc6fddf32dd310e9f114f9d38e088e699fce99
Garrett Pace <elderpace6@gmail.com>
2023-04-14 10:04:09 -0600

    learningSoMuch!

---
+++ unitOneReview.js
@@ -0,0 +1,186 @@
+// ***** Variable ***** //
+/* 
+    Create a few variables that store a value of string (letters in "", '', ``, `${}`), number, and boolean. Choose your own topic.
+*/
+
+const myName = "Garrett"
+let season = "winter"
+let color = `red`
+let number = 20
+let numberString = '20'
+console.log(myName)
+console.log(season)
+console.log(color)
+console.log(number, typeof number)
+console.log(numberString, typeof numberString)
+let todayIsCold = false
+let chiliIsSpicy = true
+let pizza = null
+console.log(todayIsCold)
+console.log(chiliIsSpicy)
+console.log(pizza)
+let food
+console.log(food)
+
+let myFavColor = `My favorite color is ${color}` // template literal
+let myFavColor2 = "My favorite color is " + color  // concatenation
+
+console.log(myFavColor)
+console.log(myFavColor2)
+
+// ***** Array ***** //
+/*
+    Create an array of strings called 'codingLanguages' that include 4 coding language of your choice. 
+*/
+
+//CODE HERE
+
+
+/* 
+    Access the 3rd element of the 'codingLanguages' array.
+*/
+
+//CODE HERE
+
+
+/*
+    Copy the array using one of the array methods, and call it 'codingLanguages2.'
+*/
+
+//CODE HERE
+
+
+/* 
+    Add another coding language to the codingLanguages2 array.
+*/
+
+//CODE HERE
+
+
+// !!! Don't edit the code below !!! //
+let instruments = ['piano', 'trumpet', 'xylophone', 'flute', 'cello']
+// !!! Don't edit the code above !!! //
+
+/* 
+    Use a built-in array function to remove 'cello' from the instruments array.
+*/
+
+//CODE HERE
+
+
+/* 
+    Use a built-in array function to remove 'piano' from the instruments array
+*/
+
+//CODE HERE
+
+
+/* 
+    Use a built-in array function to add 'guitar' to the front of the instruments array
+*/
+
+//CODE HERE
+
+
+/* 
+    Use a built-in array function to replace 'xylophone' in the instruments array to 'glockenspiel.'
+*/
+
+//CODE HERE
+
+
+
+// ***** if-else ***** //
+/* 
+    Discuss the if-else statement syntax.
+*/
+
+// !!! Don't edit the code below !!! //
+let num = 11;
+// !!! Don't edit the code above !!! //
+
+/* 
+    Refer to the num variable above.
+    If num is an even number, print num. Otherwise, print 'num is not an even number.'
+*/
+
+//CODE HERE
+
+
+// !!! Don't edit the code below !!! //
+let score = 83;
+// !!! Don't edit the code above !!! //
+/* 
+    Refer to the score variable above.
+    Using if-else statement, determine the grade of the above score.
+    If the score is less than and equal to 10, print 'Failed.'
+    If the the score is between 10 and 41, print 'The grade is C.'
+    If the the score is between 40 and 71, print 'The grade is B.'
+    If the the score is above 70, print 'The grade is A.'
+*/
+
+//CODE HERE
+
+
+
+// ***** For Loop ***** //
+/* 
+    Discuss the for loop syntax.
+*/
+
+// !!! Don't edit the code below !!! //
+let price = [10, 5, 6];
+// !!! Don't edit the code above !!! //
+
+/* 
+    Using for loop, calculate the total price from the 'price' array above.
+*/
+
+//CODE HERE
+
+
+
+/* 
+    Using the 'totalPrice,' find the average of 'price.'
+*/
+
+
+
+// !!! Don't edit the code below !!! //
+let colors = ['red', 'green', 'yellow', 'red', 'green']
+// !!! Don't edit the code above !!! //
+
+/* 
+    Loop through the colors array above. Then, print 'apple' if the color is red, print 'melon' if the color is green, and print 'banana' when the color is yellow.
+*/
+
+// CODE HERE
+
+
+
+// ***** Software Development Life Cycle (SDLC) ***** //
+/* 
+    Discuss SDLC:
+    -- planning
+    -- Analysis of Requirements
+    -- Design
+    -- Implementation
+    -- Testing & Integration
+    -- Maintain
+*/
+
+// ***** Git ***** //
+/* 
+    Discuss:
+    - Definition of:
+        -- git
+        -- github
+        -- git init
+        -- git clone
+        -- git status
+        -- git add
+        -- git commit
+        -- git push
+    - How to push git to github
+*/
+
